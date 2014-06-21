1456b1a7ca68647d42aa40bc02c9edcc13a870e0
lifetyper <leavic@gmail.com>
2014-06-21 15:50:24 +0800

    remove log to make system start more smooth

--- OpenWRT/etc/ppp/ip-down.d/vpndown.sh
+++ OpenWRT/etc/ppp/ip-down.d/vpndown.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-LOG='/tmp/v.log'
-echo "VPN Disconnected! Remove table 1.." >>$LOG
 ip rule del table 1
--- OpenWRT/etc/ppp/ip-up.d/vpnup.sh
+++ OpenWRT/etc/ppp/ip-up.d/vpnup.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
 VPN_GW=$(ip route show dev $VPN_DEV | tail -n 1 |cut -d ' ' -f 1)
-LOG='/tmp/v.log'
-echo "Got VPN Device:$VPNDEV" >>$LOG
-echo "Add IP with mark1 to table 1" >>$LOG
 ip rule add fwmark 1 table 1
-echo "Route DNS with VPN..." >>$LOG
 ip route add 8.8.8.8 dev $VPN_DEV
 ip route add 8.8.4.4 dev $VPN_DEV
 ip route add 208.67.222.222 dev $VPN_DEV
 ip route add 208.67.220.220 dev $VPN_DEV
-echo "Got VPN Gate IP:$VPN_GW" >>$LOG
-echo "Add table 1 to VPN Route" >>$LOG
 ip route add default via $VPN_GW table 1
-echo "Restart Dnsmasq" >>$LOG
 /etc/init.d/dnsmasq restart
 
