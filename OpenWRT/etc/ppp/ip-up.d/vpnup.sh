3ea120bb52dc19cd4f906a7cdfadbb2ed92935fb
lifetyper <leavic@gmail.com>
2014-06-27 12:53:36 +0800

    change dns to aliyun,add an option way to get VPN gate way

--- OpenWRT/etc/ppp/ip-up.d/vpnup.sh
+++ OpenWRT/etc/ppp/ip-up.d/vpnup.sh
@@ -3,6 +3,7 @@ LOG='/tmp/vpn.log'
 echo "VPN UP @$(date +"%T@%Y-%m-%d")" >>$LOG
 VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
 VPN_GW=$(ip route show dev $VPN_DEV | tail -n 1 |cut -d ' ' -f 1)
+#VPN_GW=$(ifconfig | grep P-t-P | sed -e "s#^[^:]*:[^:]*:\([^ ]*\).*#\1#g")
 ip route add 8.8.8.8 dev $VPN_DEV
 ip route add 8.8.4.4 dev $VPN_DEV
 ip route add 208.67.222.222 dev $VPN_DEV
