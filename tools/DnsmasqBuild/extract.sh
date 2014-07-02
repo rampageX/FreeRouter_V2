29460fc7ea9612e7e1c1f92cece0c9559777eb6a
lifetyper <leavic@gmail.com>
2014-07-02 22:18:44 +0800

    normal update

--- tools/DnsmasqBuild/extract.sh
+++ tools/DnsmasqBuild/extract.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ConfFile="./gfw.conf"
 TextFile="./GfwList.txt"
-sort $ConfFile | sed -e "s#^[^\/]*\/\([^\/]*\)\/.*\$#\1#g" >$TextFile
+sed 1d $ConfFile| sort | sed -e "s#^[^\/]*\/\([^\/]*\)\/.*\$#\1#g" >$TextFile
