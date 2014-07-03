e8c9d10d88b6102ba286ced76b918dce784db499
lifetyper <leavic@gmail.com>
2014-07-03 09:15:19 +0800

    remove live.com from local.conf due to onedrive dns poison

--- tools/DnsmasqBuild/DnsmasqBuild.sh
+++ tools/DnsmasqBuild/DnsmasqBuild.sh
@@ -85,7 +85,7 @@ for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
 do
 echo -ne "\r# Generate dnsoption for Page $i##"
 curl -s "http://www.alexa.com/topsites/countries;$i/CN" | grep "/siteinfo/"|\
-grep -vE ".cn|twitter|tumblr|google|gmail|flickr|youtube|facebook|amazon|godaddy|wikipedia" |\
+grep -vE ".cn|twitter|tumblr|google|gmail|live.com|flickr|youtube|facebook|amazon|godaddy|wikipedia" |\
 sed -e "s#^[^\/]*\/[^\/]*\/\([^\/]*\).*\".*#server=/\1/$ChinaDNS#g"\
 >>$LocalOutput
 done
