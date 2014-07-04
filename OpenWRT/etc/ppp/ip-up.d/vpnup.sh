ad674b2d7ba44f78b7bee1c0c3de288864198124
lifetyper <leavic@gmail.com>
2014-07-05 01:38:03 +0800

    update dnssec.conf,DO NOT use OpenDNS!

--- OpenWRT/etc/ppp/ip-up.d/vpnup.sh
+++ OpenWRT/etc/ppp/ip-up.d/vpnup.sh
@@ -6,8 +6,6 @@ VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
 #VPN_GW=$(ifconfig | grep P-t-P | sed -e "s#^[^:]*:[^:]*:\([^ ]*\).*#\1#g")
 ip route add 8.8.8.8 dev $VPN_DEV
 ip route add 8.8.4.4 dev $VPN_DEV
-ip route add 208.67.222.222 dev $VPN_DEV
-ip route add 208.67.220.220 dev $VPN_DEV
 #ip route add default via $VPN_GW table 1
 ip route add default dev $VPN_DEV table vpn 
 ip rule add fwmark 1 priority 1984 table vpn
