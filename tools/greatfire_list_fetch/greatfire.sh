17b0595122aea2bbd31f15e6bb728a46e378ce80
lifetyper <leavic@gmail.com>
2014-07-07 14:13:20 +0800

    tiny change in regex expression

--- tools/greatfire_list_fetch/greatfire.sh
+++ tools/greatfire_list_fetch/greatfire.sh
@@ -10,7 +10,7 @@ for i in 0 1 2 3 4 5 6 7 8 9
 do
 curl -s --insecure "https://zh.greatfire.org/search/alexa-top-1000-domains?page=$i"|  \
 grep 'class="first"' | grep 'class="blocked"' | grep -vE "google"|\
-sed -e "s#^[^\/]*\/\([^\"]*\)[^\%]*\%.\{3\}\([^\%]*\)\%.*#\1 \2#g"|\
+sed -e "s#^[^\/]*\/\([^\"]*\)[^\%]*\%.\{3\}\([^\D]*\)\%.*#\1 \2#g"|\
 #awk '$2>='"$Threshold"' {printf $1 "\t" $2 "\n"}' 
 awk '$2>='"$Threshold"' {print "ipset=/"$1"/vpn" }'\
 >>$Output
