94c898e2f282ed554ee427574883b8a7430731d8
lifetyper <leavic@gmail.com>
2014-07-14 13:10:11 +0800

    Big Change On DNS Solution
    改用string模块进行DNS解析结果净化,这样对所有的DNS服务器都有效,因此可以不再需要VPN支持DNS服务器,让本地DNS和境外DNS同时工作,返回最快且确保干净的解析结果.

--- OpenWRT/etc/ppp/ip-up.d/vpnup.sh
+++ OpenWRT/etc/ppp/ip-up.d/vpnup.sh
@@ -2,10 +2,10 @@
 LOG='/tmp/vpn.log'
 echo "VPN UP @$(date +"%T@%Y-%m-%d")" >>$LOG
 VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
-echo "Add DNS servers to VPN route @$(date +"%T@%Y-%m-%d")" >>$LOG
-cat /etc/resolv.conf |grep -vE "#|127.0.0.1"|sed -e "s#^[^ ]* \([^ ]*\).*\$#route add -host \1 dev "$VPN_DEV"#g" >>dns_route.sh
-/bin/sh dns_route.sh
-rm dns_route.sh
+#echo "Add DNS servers to VPN route @$(date +"%T@%Y-%m-%d")" >>$LOG
+#cat /etc/resolv.conf |grep -vE "#|127.0.0.1"|sed -e "s#^[^ ]* \([^ ]*\).*\$#route add -host \1 dev "$VPN_DEV"#g" >>dns_route.sh
+#/bin/sh dns_route.sh
+#rm dns_route.sh
 echo "Add VPN device for table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG
 ip route add default dev $VPN_DEV table vpn 
 echo "Add IPs marked by firewall to table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG  
