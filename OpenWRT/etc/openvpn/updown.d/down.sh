6a83894d1bd908c9c5d81d673870eb96f1c8a907
lifetyper <leavic@gmail.com>
2014-08-02 15:20:53 +0800

    添加OpenVPN支持，手册跟进更新
    请你自己先参考手册解决OpenVPN的连接问题再考虑后面的部署，OpenVPN本身的连接建立和FreeRouter V2毫无关系。

---
+++ OpenWRT/etc/openvpn/updown.d/down.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+LOG='/tmp/vpn.log'
+echo "VPN disconnect@$(date +"%T@%Y-%m-%d")" >>$LOG
+echo "Remove IP rules for table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG
+ip rule del table vpn
--- OpenWRT/etc/openvpn/updown.d/up.sh
+++ OpenWRT/etc/openvpn/updown.d/up.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#! /bin/sh
 LOG='/tmp/vpn.log'
 echo "VPN UP @$(date +"%T@%Y-%m-%d")" >>$LOG
-VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
+VPN_DEV=tun0
 echo "Add VPN device for table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG
-ip route add default dev $VPN_DEV table vpn 
-echo "Add IPs marked by firewall to table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG  
+ip route add default dev $VPN_DEV table vpn
+echo "Add IPs marked by firewall to table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG
 ip rule add fwmark 1 priority 1984 table vpn
 /etc/init.d/dnsmasq restart
